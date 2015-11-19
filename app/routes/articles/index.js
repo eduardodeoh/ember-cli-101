c36840c2b21dbc6ab1f02ca363591002fd546c14
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2015-11-18 23:16:00 -0200

    mockup com ember componente

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -3,5 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.modelFor('friends/show').get('articles');
-  }
+  },
+  actions: {
+    save(model) {
+      model.save();
+      return false;
+    }
+  },
+  // Problemas aqui :|
+  // resetController(controller, isExiting) {
+  //   if (isExiting) {
+  //     var model = this.modelFor('friends/show').get('articles');
+  //     model.rollback();
+  //   }
+  // }
 });
