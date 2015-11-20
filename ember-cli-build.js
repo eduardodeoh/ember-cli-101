01dc6c1d65563180da42e0ef422596e50d938b4b
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2015-11-20 21:52:06 -0200

    working with javascript plugins

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -30,5 +30,8 @@ module.exports = function(defaults) {
   app.import('bower_components/picnic/releases/plugins.min.css');
   app.import('bower_components/picnic/releases/picnic.min.css');
 
+  //Moment.js lib
+  app.import('bower_components/moment/moment.js');
+
   return app.toTree();
 };
