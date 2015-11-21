32f791a83196c322a3b7142f62437c76af32bcd6
Eduardo de O. Hernandes <eduardodeoh@gmail.com>
2015-11-21 14:01:26 -0200

    [WIP] Testing

--- tests/unit/models/friend-test.js
+++ tests/unit/models/friend-test.js
@@ -1,12 +1,33 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('friend', 'Unit | Model | friend', {
+moduleForModel('friend', 'Friend', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:article']
 });
 
 test('it exists', function(assert) {
   var model = this.subject();
-  // var store = this.store();
-  assert.ok(!!model);
+
+  assert.ok(model);
 });
+
+test('fullName joins first and last name', function(assert) {
+  var model = this.subject({firstName: 'Syd', lastName: 'Barret'});
+
+  assert.equal(model.get('fullName'), 'Syd Barret', 'Updates fullName test 1');
+
+  Ember.run(function() {
+    model.set('firstName', 'Geddy');
+  });
+
+  assert.equal(model.get('fullName'), 'Geddy Barret', 'Updates fullName test 2');
+});
+
+test('articles relationship', function(assert) {
+  var klass = this.subject({}).constructor;
+  var relationship = Ember.get(klass, 'relationshipsByName').get('articles');
+
+  assert.equal(relationship.key, 'articles');
+  assert.equal(relationship.kind, 'hasMany');
+})
+
